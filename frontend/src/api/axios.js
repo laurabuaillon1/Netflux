957c01bfed73f123f3bd67c58c7b08a4deabb281
Laura Buaillon <laura.buaillon1@gmail.com>
2026-06-09 16:49:07 +0200

    feat: initialisation frontend Vue.js

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://localhost:8000/api",
+});
+
+// Intercepteur pour ajouter le token JWT
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
+
